8904ad00d9250e41044ad6d518a3e2f618acc6e4
Trevor Jameson <tjameson15@gmail.com>
2018-07-13 15:24:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,12 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|dwarf, index|
-    puts"#{index + 1}. #{dwarf}"
+    puts "#{index + 1}. #{dwarf}"
   }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteers.collect! |power|
+    "#{power.capitalize}!"
 end
 
 def long_planeteer_calls# code an argument here
