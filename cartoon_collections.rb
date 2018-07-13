2c675be003dadcab6e9ccfefa3c9abc636119dd5
Trevor Jameson <tjameson15@gmail.com>
2018-07-13 15:39:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,9 +19,17 @@ def long_planeteer_calls(calls)
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.each do |cheese|
+    cheese_types do |type|
+      if cheese === type
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Test dangerous method "collect!"
