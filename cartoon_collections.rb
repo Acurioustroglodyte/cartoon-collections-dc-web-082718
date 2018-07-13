280f6db4d9834dd2ded9e14ae0738063a9e4181f
Trevor Jameson <tjameson15@gmail.com>
2018-07-13 15:44:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,28 +19,28 @@ def long_planeteer_calls(calls)
   return false
 end
 
-#def find_the_cheese(cheeses)
-  # the array below is here to help
-#  cheese_types = ["cheddar", "gouda", "camembert"]
-#  cheeses.each do |cheese|
-#    cheese_types.each do |type|
-#      if cheese === type
-#        return cheese
-#      end
-#    end
-#  end
-#  return nil
-#end
-
 def find_the_cheese(cheeses)
+  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheeses.each do |cheese|
-    if cheese_types.include?(cheese)
-      return cheese
+    cheese_types.each do |type|
+      if cheese === type
+        return cheese
+      end
     end
   end
   return nil
 end
+
+#def find_the_cheese_include(cheeses)
+#  cheese_types = ["cheddar", "gouda", "camembert"]
+#  cheeses.each do |cheese|
+#    if cheese_types.include?(cheese)
+#      return cheese
+#    end
+#  end
+#  return nil
+#end
 
 # Test dangerous method "collect!"
 # planet = ["earth", "wind", "fire", "water", "heart"]
