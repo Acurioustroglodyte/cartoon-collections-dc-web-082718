f52083cd327dbc4146b40d9d371d3db797529f65
Trevor Jameson <tjameson15@gmail.com>
2018-07-13 15:43:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,14 +19,24 @@ def long_planeteer_calls(calls)
   return false
 end
 
-def find_the_cheese(cheeses)
+#def find_the_cheese(cheeses)
   # the array below is here to help
+#  cheese_types = ["cheddar", "gouda", "camembert"]
+#  cheeses.each do |cheese|
+#    cheese_types.each do |type|
+#      if cheese === type
+#        return cheese
+#      end
+#    end
+#  end
+#  return nil
+#end
+
+def find_the_cheese_include(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheeses.each do |cheese|
-    cheese_types.each do |type|
-      if cheese === type
-        return cheese
-      end
+    if cheese_types.include(cheese)
+      return cheese
     end
   end
   return nil
